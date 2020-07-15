53e60f9f55240b2f120a06a42e6d6bce4f8930a3
shadz22 <shadz22@yahoo.co.uk>
2020-07-15 21:48:10 +0100

    remove nil and false from an array

--- lib/questions.rb
+++ lib/questions.rb
@@ -1,17 +1,22 @@
 # keep only the elements that start with an a
 def select_elements_starting_with_a(array)
+  # array.grep(/^a/)
+  array.select {|element| element.start_with?('a') }
 end
 
 # keep only the elements that start with a vowel
 def select_elements_starting_with_vowel(array)
+  array.grep(/^aeiou/)
 end
 
 # remove instances of nil (but NOT false) from an array
 def remove_nils_from_array(array)
+  array.compact
 end
 
 # remove instances of nil AND false from an array
 def remove_nils_and_false_from_array(array)
+  array.delete_if {|element| element == nil || element == false}
 end
 
 # don't reverse the array, but reverse every word inside it. e.g.
--- spec/questions_spec.rb
+++ spec/questions_spec.rb
@@ -8,7 +8,7 @@ end
 
 describe 'the Friday test :)' do
 
-  it 'select_elements_starting_with_a' do
+  fit 'select_elements_starting_with_a' do
     n = select_elements_starting_with_a ['bananas', 'apples', 'pears', 'avocados']
     expect(n).to eq ['apples', 'avocados']
   end
@@ -18,12 +18,12 @@ describe 'the Friday test :)' do
     expect(n).to eq ['omar', 'idris', 'angela']
   end
 
-  it 'remove_nils_from_array' do
+  fit 'remove_nils_from_array' do
     n = remove_nils_from_array ['a', 'b', nil, nil, false, 'c', nil]
     expect(n).to eq ['a', 'b', false, 'c']
   end
 
-  it 'remove_nils_and_false_from_array' do
+  fit 'remove_nils_and_false_from_array' do
     n = remove_nils_and_false_from_array ['a', 'b', nil, nil, false, 'c', nil]
     expect(n).to eq ['a', 'b', 'c']
   end
