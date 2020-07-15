c302717b05cd3e630153cbaccfef4d7b309de2fd
shadz22 <shadz22@yahoo.co.uk>
2020-07-15 22:11:47 +0100

    reverse each element in an array

--- lib/questions.rb
+++ lib/questions.rb
@@ -22,6 +22,7 @@ end
 # don't reverse the array, but reverse every word inside it. e.g.
 # ['dog', 'monkey'] becomes ['god', 'yeknom']
 def reverse_every_element_in_array(array)
+  array.map(&:reverse!)
 end
 
 # given an array of student names, like ['Bob', 'Dave', 'Clive']
